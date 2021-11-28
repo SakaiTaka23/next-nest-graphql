1f18751e3aa61d33c095c3da38ff1c2c6658ba66
SakaiTaka23 <tt3.sakai@gmail.com>
2021-11-28 15:08:35 +0900

    graphqlの設定をした

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: 'http://127.0.0.1:5000/graphql',
+    cache: new InMemoryCache(),
+    connectToDevTools: true,
+  });
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp;
+export default App;

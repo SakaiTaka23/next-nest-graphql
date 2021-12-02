b09805141cc9f8ea131a320348967aa93e7860d4
SakaiTaka23 <tt3.sakai@gmail.com>
2021-12-02 20:26:58 +0900

    クエリを更新

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,22 +1,18 @@
-import { Container, Typography } from '@mui/material';
 import type { NextPage } from 'next';
-import { useUsersQuery } from '../graphql/generated';
+import { useUserFromIdQuery } from '../graphql/generated';
 
 const Home: NextPage = () => {
-  const { data, loading, error } = useUsersQuery();
+  const { data, loading, error } = useUserFromIdQuery({
+    variables: {
+      userId: '1',
+    },
+  });
 
   if (loading) return <p>Loading...</p>;
   console.log(error);
   if (error) return <p>Error :(</p>;
 
-  return (
-    <Container maxWidth='sm'>
-      <Typography variant='h2'>users</Typography>
-      {data?.users.map((user) => {
-        return <div key={user?.id}>{user?.id}</div>;
-      })}
-    </Container>
-  );
+  return <div>{`user id : ${data?.user?.id}`}</div>;
 };
 
 export default Home;
--- frontend/pages/private.tsx
+++ frontend/pages/private.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useUserFromTokenQuery } from '../graphql/generated';
 
 const Private = () => {
-  return <div>private</div>;
+  const { data, loading, error } = useUserFromTokenQuery();
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return <div>{`user id : ${data?.userFromToken.id}`}</div>;
 };
 
 export default Private;

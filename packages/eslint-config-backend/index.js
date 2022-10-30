995f5bcaf0fed3d284fa435d89b2e77e666c19f8
SakaiTaka23 <tt3.sakai@gmail.com>
2022-10-30 14:29:11 +0900

    バックのlint設定追加

---
+++ packages/eslint-config-backend/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['turbo', 'airbnb-base', 'airbnb-typescript/base', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+  },
+};

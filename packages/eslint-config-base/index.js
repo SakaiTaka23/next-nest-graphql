2900ff41cb09c620675997da82f16f081d07b415
SakaiTaka23 <tt3.sakai@gmail.com>
2022-11-05 16:15:24 +0900

    import周りのプラグイン追加

--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -1,3 +1,26 @@
 module.exports = {
   extends: ['turbo', 'prettier'],
+  plugins: ['import', 'unused-imports'],
+  rules: {
+    'unused-imports/no-unused-imports': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };

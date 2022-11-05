6a6333d8bb9ebfa183635d1ef7b4e14019e9c10f
SakaiTaka23 <tt3.sakai@gmail.com>
2022-11-05 15:19:51 +0900

    不要なルールを削除

--- packages/eslint-config-frontend/index.js
+++ packages/eslint-config-frontend/index.js
@@ -7,6 +7,14 @@ module.exports = {
   },
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/jsx-props-no-spreading': 'off',
     'react/jsx-key': 'off',
   },
 };

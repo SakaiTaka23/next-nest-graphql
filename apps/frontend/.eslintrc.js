6d2c1abe97439d9cd2e89fd22ed1a3798045030b
SakaiTaka23 <tt3.sakai@gmail.com>
2022-11-05 15:20:03 +0900

    tsconfig取り込み

--- apps/frontend/.eslintrc.js
+++ apps/frontend/.eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: ['custom-frontend'],
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: ['.eslintrc.js'],
 };
